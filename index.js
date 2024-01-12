95a487e6e30f5434bf91b272c75522c50d0bffd4
Arun Prasath <98601481+arunprasathweb@users.noreply.github.com>
2024-01-12 14:04:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let title =[];
+let content =[];
+
+
+app.get('/', (req, res) => {
+    res.render('home.ejs', { title });
+});
+
+
+app.get("/posts/:id", (req, res) => {
+    const { id } = req.params;
+    const Ntitle = title[id];
+    const Ncontent = content[id];
+
+    res.render("post.ejs", { Ntitle, Ncontent});
+  });
+
+  app.post("/create-post", (req, res) => {
+    title.push(req.body.title);
+    content.push(req.body.content);
+    res.redirect("/");
+});  
+
+
+
+
+  
+
+
+
+
+app.listen(port, () => {
+    console.log('App is up and runing');
+});
